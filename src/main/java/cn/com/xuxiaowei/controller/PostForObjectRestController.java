3120ada32ca015cf3c134965b6981e6874b51567
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-01-26 19:59:49 +0800

    Post 根据 URL（{@link URI}）、实体类解析返回结果

--- src/main/java/cn/com/xuxiaowei/controller/PostForObjectRestController.java
+++ src/main/java/cn/com/xuxiaowei/controller/PostForObjectRestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Spring Post 使用示例
@@ -26,7 +27,7 @@ public class PostForObjectRestController {
      * @param request
      * @param response
      * @return
-     * @see RestTemplate#postForObject(URI, Object, Class)  字符串类型的 URL
+     * @see RestTemplate#postForObject(String, Object, Class, Object...)   字符串类型的 URL
      */
     @PostMapping("/postForObject1")
     public User postForObject1(HttpServletRequest request, HttpServletResponse response) {
@@ -45,5 +46,29 @@ public class PostForObjectRestController {
         return postUser1;
     }
 
+    /**
+     * Post 根据 URL（{@link URI}）、实体类解析返回结果
+     *
+     * @param request
+     * @param response
+     * @return
+     * @see RestTemplate#postForObject(URI, Object, Class)  字符串类型的 URL
+     */
+    @PostMapping("/postForObject2")
+    public User postForObject2(HttpServletRequest request, HttpServletResponse response) throws URISyntaxException {
+
+        // 创建 RestTemplate 示例
+        RestTemplate restTemplate = new RestTemplate();
+
+        // URL
+        String url = request.getRequestURL().toString().replace(request.getRequestURI(), "/postUser1");
+
+        // 使用指定实体类解析返回结果
+        User postUser1 = restTemplate.postForObject(new URI(url), null, User.class);
+
+        log.debug(String.valueOf(postUser1));
+
+        return postUser1;
+    }
 
 }
