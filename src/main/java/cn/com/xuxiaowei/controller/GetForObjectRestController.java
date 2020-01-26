fcec525d7bc4554176bb16361e7209e8ffc274df
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-01-26 19:22:04 +0800

    Get 根据 URL（{@link URI}）、实体类解析返回结果

--- src/main/java/cn/com/xuxiaowei/controller/GetForObjectRestController.java
+++ src/main/java/cn/com/xuxiaowei/controller/GetForObjectRestController.java
@@ -8,6 +8,8 @@ import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Spring Get 使用示例
@@ -20,11 +22,12 @@ import javax.servlet.http.HttpServletResponse;
 public class GetForObjectRestController {
 
     /**
-     * Get 根据实体类解析返回结果
+     * Get 根据 URL（字符串）、实体类解析返回结果
      *
      * @param request
      * @param response
      * @return
+     * @see RestTemplate#getForObject(String, Class, Object...) 字符串类型的 URL
      */
     @GetMapping("/getForObject1")
     public User getForObject1(HttpServletRequest request, HttpServletResponse response) {
@@ -43,5 +46,29 @@ public class GetForObjectRestController {
         return getUser1;
     }
 
+    /**
+     * Get 根据 URL（{@link URI}）、实体类解析返回结果
+     *
+     * @param request
+     * @param response
+     * @return
+     * @see RestTemplate#getForObject(URI, Class) {@link URI} 类型的 URL
+     */
+    @GetMapping("/getForObject2")
+    public User getForObject2(HttpServletRequest request, HttpServletResponse response) throws URISyntaxException {
+
+        // 创建 RestTemplate 示例
+        RestTemplate restTemplate = new RestTemplate();
+
+        // URL
+        String url = request.getRequestURL().toString().replace(request.getRequestURI(), "/getUser1");
+
+        // 使用指定实体类解析返回结果
+        User getUser1 = restTemplate.getForObject(new URI(url), User.class);
+
+        log.debug(String.valueOf(getUser1));
+
+        return getUser1;
+    }
 
 }
