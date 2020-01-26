55e4c1b14695f9d3ed5404e6083c8ad26c0ed591
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-01-26 21:26:28 +0800

    Slf4j

--- src/main/java/cn/com/xuxiaowei/controller/IndexController.java
+++ src/main/java/cn/com/xuxiaowei/controller/IndexController.java
@@ -1,5 +1,6 @@
 package cn.com.xuxiaowei.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
  * @author xuxiaowei
  * @since 0.0.1
  */
+@Slf4j
 @Controller
 public class IndexController {
 
