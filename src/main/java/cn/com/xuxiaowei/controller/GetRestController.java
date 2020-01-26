fce0fdaa53169c926de53cac6b3e4b6b7f74e3c0
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-01-26 19:47:09 +0800

    Slf4j

--- src/main/java/cn/com/xuxiaowei/controller/GetRestController.java
+++ src/main/java/cn/com/xuxiaowei/controller/GetRestController.java
@@ -1,6 +1,7 @@
 package cn.com.xuxiaowei.controller;
 
 import cn.com.xuxiaowei.entity.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import java.util.concurrent.atomic.AtomicLong;
  * @author xuxiaowei
  * @since 0.0.1
  */
+@Slf4j
 @RestController
 public class GetRestController {
 
@@ -34,15 +36,18 @@ public class GetRestController {
      *
      * @param request
      * @param response
+     * @param username 参数
      * @return 返回实体类，使用 jackson 将 {@link User} 转换为 JSON
      * @see <a href="https://github.com/FasterXML/jackson">jackson</a>
      */
     @GetMapping(value = {"/getUser1"})
-    public User getUser1(HttpServletRequest request, HttpServletResponse response) {
+    public User getUser1(HttpServletRequest request, HttpServletResponse response, String username) {
+        log.debug("username：{}", username);
         User user = new User();
         user.setUserId(counter.incrementAndGet());
         user.setUsername("徐晓伟");
         user.setPassword(UUID.randomUUID().toString().replace("-", ""));
+        log.debug("user：{}", user);
         return user;
     }
 
--- src/main/java/cn/com/xuxiaowei/controller/PostRestController.java
+++ src/main/java/cn/com/xuxiaowei/controller/PostRestController.java
@@ -1,6 +1,7 @@
 package cn.com.xuxiaowei.controller;
 
 import cn.com.xuxiaowei.entity.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ import java.util.concurrent.atomic.AtomicLong;
  * @author xuxiaowei
  * @since 0.0.1
  */
+@Slf4j
 @RestController
 public class PostRestController {
 
@@ -28,15 +30,18 @@ public class PostRestController {
      *
      * @param request
      * @param response
+     * @param username 参数
      * @return 返回实体类，使用 jackson 将 {@link User} 转换为 JSON
      * @see <a href="https://github.com/FasterXML/jackson">jackson</a>
      */
     @PostMapping(value = {"/postUser1"})
-    public User postUser1(HttpServletRequest request, HttpServletResponse response) {
+    public User postUser1(HttpServletRequest request, HttpServletResponse response, String username) {
+        log.debug("username：{}", username);
         User user = new User();
         user.setUserId(counter.incrementAndGet());
         user.setUsername("徐晓伟");
         user.setPassword(UUID.randomUUID().toString().replace("-", ""));
+        log.debug("user：{}", user);
         return user;
     }
 
