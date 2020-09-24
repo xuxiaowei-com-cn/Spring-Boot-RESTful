8f0ffccec707ce1f8ea8d00a9c2f494a13668cbd
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-09-24 21:23:15 +0800

    修复输入流为空读取异常

--- src/main/java/cn/com/xuxiaowei/util/RequestUtils.java
+++ src/main/java/cn/com/xuxiaowei/util/RequestUtils.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package cn.com.xuxiaowei.util;
 
 import javax.servlet.ServletInputStream;
@@ -39,10 +54,15 @@ public class RequestUtils {
      */
     public static String getInputStream(HttpServletRequest request) throws IOException {
         ServletInputStream inputStream = request.getInputStream();
-        byte[] bytes = new byte[request.getContentLength()];
-        String characterEncoding = request.getCharacterEncoding();
-        inputStream.read(bytes);
-        return new String(bytes, characterEncoding);
+        int contentLength = request.getContentLength();
+        if (contentLength == -1) {
+            return null;
+        } else {
+            byte[] bytes = new byte[contentLength];
+            String characterEncoding = request.getCharacterEncoding();
+            inputStream.read(bytes);
+            return new String(bytes, characterEncoding);
+        }
     }
 
 }
